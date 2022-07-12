1d4daae1f520c87724fc249c99cbf9afce0b7448
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-12 20:38:34 +0800

    Adds basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritesPage from './pages/Favourites';
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/fav'>
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

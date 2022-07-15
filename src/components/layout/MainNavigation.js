fdae7a54ac87b83bc63dab3b89d16b6551a4295d
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-15 21:04:38 +0800

    Adds badge on main navigation for keeping track of no. of favourited items

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import FavouritesContext from '../../store/favourites-context';
 
 function MainNavigation() {
+  const favouriteCtx = useContext(FavouritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function MainNavigation() {
             <Link to='/new'>Add New Meetup</Link>
           </li>
           <li>
-            <Link to='/fav'>Favourites</Link>
+            <Link to='/fav'>
+              Favourites
+              <span className={classes.badge}>
+                {favouriteCtx.totalFavourites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
